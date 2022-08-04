4fb7810a80dea2584e7a91a6f43443a7cda40f84
dffos <1251970841@qq.com>
2022-08-05 01:20:22 +0800

    初始化项目完成

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 引入样式
+import './styles/base.less'
+// 导入vant组件
+import './utils/vant'
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/MyLogin/index.vue
+++ src/views/MyLogin/index.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <router-view/>
+  
   </div>
 </template>
 
 <script>
 export default {
-  
+
 }
 </script>
 
